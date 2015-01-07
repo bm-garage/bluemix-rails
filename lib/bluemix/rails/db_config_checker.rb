52d8217165549a0ca5d829e5447456ca400165f3
Steven Magelowitz <steven.magelowitz@gmail.com>
2015-01-07 15:37:24 -0800

    Removes production from array

--- lib/bluemix/rails/db_config_checker.rb
+++ lib/bluemix/rails/db_config_checker.rb
@@ -4,4 +4,23 @@ class DbConfigChecker
   def initialize(file_location)
     @yml_file = File.open(file_location).read.split(/\n/)
   end
+
+  def delete_existing_production!
+    delete = false
+
+    yml_file.select {|line|
+
+      if delete && !line.start_with?("  ")
+        delete = false
+      end
+
+      if line.start_with?("production:")
+        delete = true
+      end
+
+      if !delete
+        line
+      end
+    }
+  end
 end
